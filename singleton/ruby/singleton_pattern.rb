696d4031c179aac9a3c1fcbfb098b74bd7eaa500
Vladislav Trotsenko <admin@bestweb.com.ua>
2019-07-01 16:55:38 +0300

    Complete Singleton pattern

--- singleton/ruby/singleton_pattern.rb
+++ singleton/ruby/singleton_pattern.rb
@@ -3,12 +3,51 @@
 # 'Ensure a class only has one instance, and provide a global point
 # of access to it'
 
+# Not thread safe singleton
+
 class Singleton
+  private_class_method :new
+
   def self.instance
     @instance ||= new
   end
 
-  private_class_method :new
+  attr_accessor :data
+
+  def initialize
+    @data = :data
+  end
 end
 
-Singleton.instance
+# Thread safe singleton
+
+class Singleton
+  private_class_method :new
+
+  def self.instance
+    @instance ||= new
+  end
+
+  def initialize
+    @data = :data
+    @mutex = Mutex.new
+  end
+
+  def data
+    with_mutex { @data }
+  end
+
+  def data=(arg)
+    with_mutex { @data = arg }
+  end
+
+  private
+
+  def with_mutex
+    @mutex.synchronize { yield }
+  end
+end
+
+p Singleton.instance
+Singleton.instance.data = :new_data
+p Singleton.instance
